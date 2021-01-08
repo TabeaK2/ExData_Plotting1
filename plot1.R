e201260b2215398450166c044347055093ad7abf
TabeaK2 <jennifer.koenitzer@gmail.com>
2021-01-08 17:44:29 +0000

    Plot 1 finsihed

--- plot1.R
+++ plot1.R
@@ -1,8 +1,7 @@
-library(readr)
 url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 download.file(url,"epower.zip") #Download zip file.
 unzip("epower.zip", "household_power_consumption.txt") #Unzip file.
-epower<-read.csv2("household_power_consumption.txt") #Create dataframe using Readr "read.csv2" package.
+epower<-read.table("household_power_consumption.txt", header = TRUE, sep=";", stringsAsFactors = FALSE) #Create dataframe using Readr "read.csv2" package.
 #Wrong Column classes, change to appropriate class.
 epower$Date <- as.Date(epower$Date, format ="%d/%m/%Y")
 epower$Global_active_power <- as.numeric(epower$Global_active_power)
@@ -12,4 +11,11 @@ epower$Global_intensity <- as.numeric(epower$Global_intensity)
 epower$Sub_metering_1 <- as.numeric(epower$Sub_metering_1)
 epower$Sub_metering_2 <- as.numeric(epower$Sub_metering_2)
 epower$Sub_metering_3 <- as.numeric(epower$Sub_metering_3)
+#Subset data for 2 selected dates.
 epowersubset<-subset(epower, Date =="2007-02-01" | Date =="2007-02-02")
+#Initiate png.
+png("plot1.png", width=480, height=480)
+#Create histogram.
+hist(epowersubset$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power(kilowatts)")
+#Copy histogram to png and save.
+dev.off()
